437dcd55915873c90a12e6a71175204c759d150e
ihoyai <100013537+ihoyai@users.noreply.github.com>
2022-12-14 10:27:15 +0900

    Update hardware.h
    20%

--- selfdrive/hardware/eon/hardware.h
+++ selfdrive/hardware/eon/hardware.h
@@ -24,7 +24,8 @@ public:
   static void poweroff() { std::system("LD_LIBRARY_PATH= svc power shutdown"); };
   static void set_brightness(int percent) {
 
-
+    
+    percent= 20;
     std::ofstream brightness_control("/sys/class/leds/lcd-backlight/brightness");
     
     if (brightness_control.is_open()) {
